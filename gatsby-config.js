26c1d508d68aad38818c534757a00d9608068b9a
taketoyanagihara <51067995+taketoyanagihara@users.noreply.github.com>
2023-02-01 21:46:45 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `https://yanagiharataketo.com/graphql`,
+          `http://post.yanagiharataketo.com/graphql`,
       },
     },
 
