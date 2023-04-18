f6c6e5deb79532ed31f39a8046ef0dfed5b4174f
yanagiharataketo <yanagihara.taketo@gmail.com>
2023-04-18 22:38:20 +0900

    font re

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,11 +35,7 @@ const Layout = ({ isHomePage, children }) => {
       <main>{children}</main>
 
       <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
-        {` `}
-        And <a href="https://wordpress.org/">WordPress</a>
+        © {new Date().getFullYear()} {parse(title)}
       </footer>
     </div>
   )
